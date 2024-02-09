98ec5fda2613dd878454e3da2d7d5602c2bed188
Joshua Burnett <joshuaburnett429@gmail.com>
2024-02-09 16:38:19 -0500

    fixing deployment dependencies

--- src/components/terminal/terminal.spec.ts
+++ src/components/terminal/terminal.spec.ts
@@ -1,18 +1,32 @@
-import React, { Component } from "react";
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import TerminalComponent from "./terminal";
 
 describe('TerminalComponent', () => {
-    beforeEach(() => {
-        render(TerminalComponent())
+    it('renders to screen with correct welcome message', () => {
+        render(TerminalComponent());
+        const welcomeMessage = screen.getByText(`Welcome to my Porfolio, to view my resume please type "resume" otherwise please feel free to explore the house by having a "lookAround"! If you get stuck, ask for "help".`);
+        expect(welcomeMessage).toBeInTheDocument(); 
     })
-    it('renders to screen properly', () => {
-        const { getByText } = render(TerminalComponent());
-        const message = getByText('Welcome to my Porfolio, to view my resume please type "resume" otherwise please feel free to explore the house by having a "lookAround"! If you get stuck, ask for "help".');
-        expect(message).toBeInTheDocument(); 
+    it('renders with correct prompt', () => {
+        render(TerminalComponent());
+        const prompt = screen.getByText('>> what would you like to do?')
+        expect(prompt).toBeInTheDocument();
     })
     it('can be typed into', () => {
-        userEvent.t
+        render(TerminalComponent());
+        const inputField = document.getElementsByClassName('index_lineText__j6qNj')[0];
+        userEvent.type(inputField, 'lookAround');
+        const check = screen.getByText('lookAround');
+        expect(check).toBeInTheDocument();
     })
+    // it('handles the lookAround input', () => {
+    //     render(TerminalComponent());
+    //     const inputField = document.getElementById('terminalEditor');
+    //     userEvent.click(inputField);
+    //     userEvent.type(inputField, 'lookAround');
+    //     userEvent.type(inputField, `{enter}`)
+    //     const roomDescription = screen.getByText(`You're in a small office. Before you stands a large Mahogany desk adorned with a distastefully bright RGB Keyboard going full rainbow vomit mode and an equally sickening computer to match. Two large monitors sit on the desk one displaying a YouTube video and another with a resume on screen. The walls are littered with the "hang in there" posters you'd find in a high school. Directly behind the desk are three plaques on the wall.`);
+    //     expect(roomDescription).toBeInTheDocument();
+    // })
 })
